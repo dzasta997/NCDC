24c7fa05701d48736a94ee59eb6bd7c4661c6318
Justyna <justynaturowska97@gmail.com>
2021-06-10 00:53:37 +0200

    bodies

--- src/ncdc/Bodies.java
+++ src/ncdc/Bodies.java
@@ -21,14 +21,16 @@ public void add(Body body) {
 	}
 }
 public String toString() {
-	
-	
-	return table.toString();
+	String str = "";
+	for(Map.Entry<Double,Body> body:table.entrySet()) {
+		str+=table.get(body.getKey()).name+", ";
+	}
+	return str;
 }
 public Bodies() {
 	this.table = new TreeMap<Double,Body>() ;
 }
-public SortedMap<Double,Body> countBodies(double day) {
+public Bodies  countBodies(double day) {
 	Bodies res = new Bodies();
 	res.add(this.center);
 	for(Map.Entry<Double,Body> body:table.entrySet()) {
@@ -36,15 +38,15 @@ public SortedMap<Double,Body> countBodies(double day) {
 		b.position = b.calculatePosition(day);
 		res.add(b);
 	}
-	return res.table;
+	return res;
 }
 public static void main(String [] args) {
 	Bodies bodies = new Bodies();
 	bodies.add(new Body("Mars",0,0));
 	bodies.add(new Body("Venus",4,1));
 	bodies.add(new Body("Sun",24,2));
-
+	Bodies boo = bodies.countBodies(75);
 	//bodies.countBodies(3);
-	System.out.print(bodies.countBodies(75).toString());
+	System.out.print(boo.toString());
 }
 }
