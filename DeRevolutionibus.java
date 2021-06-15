5cad1a1fafab68e29ad064a53e3f409389382a42
Justyna <justynaturowska97@gmail.com>
2021-06-15 16:53:02 +0200

    argument reading exception handled

--- DeRevolutionibus.java
+++ DeRevolutionibus.java
@@ -119,17 +119,12 @@ public class DeRevolutionibus {
 	}
 public static void main ( String [] args) 	throws IOException{
 
-    
-//        // Enter data using BufferReader
-//        BufferedReader reader = new BufferedReader(
-//            new InputStreamReader(System.in));
-// 
-//        // Reading data using readLine
-//        String name = reader.readLine();
-// 
-//        // Printing the read line
-//        System.out.println(name);
-    
-	new DeRevolutionibus(Double.parseDouble(args[0]));
+if(args.length==0) {
+    	System.out.println("klops");
+    }
+    else {
+    	new DeRevolutionibus(Double.parseDouble(args[0]));
+    }
+
 }
 }
